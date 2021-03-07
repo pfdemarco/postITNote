2fb94b36f7baa7a92acf19b0603ec59c3d35c3fc
Patrick DeMarco <pfdemarco@hotmail.com>
2021-03-07 10:19:39 -0500

    changed readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Note Taker via express
+
+## Note Taker
+
+Modify starter code to create an application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+The application’s front end has already been created. It's your job to build the back end, connect the two, and then deploy the entire application to Heroku.
+
+## Picture of Main Notes pg
+
+![Notes Main](NotesMain.jpg)
+
+
+## Picture of Note pg
+
+![Notes Note](NotesNote.jpg)
+
+## Link to deployed Heroku page
+[Deployed Page](https://pfdpostitnote.herokuapp.com/notes)
+
+## Link to Github repo
+[Github repo](https://pfdemarco.github.io/postITNote/)
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,8 +7,11 @@ const uuid = require('uuid');
 router.get('/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
-        const getNotes = JSON.parse(data);
-        res.json(getNotes);
+        const getNotes = JSON.parse(data);//The JSON.parse() method parses a JSON string, constructing the JavaScript value or object described by the string. 
+                                          //An optional reviver function can be provided to perform a transformation on the resulting object before it is returned.
+        res.json(getNotes);//The res.json() function sends a JSON response. 
+                           //This method sends a response (with the correct content-type) 
+                           //that is the parameter converted to a JSON string using the JSON.stringify() method.
     });
 });
 
@@ -23,10 +26,12 @@ router.post('/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
         const postNote = JSON.parse(data);
-        postNote.push(postITNote);
-        fs.writeFile('./db/db.json', JSON.stringify(postNote), err => {
+        postNote.push(postITNote);//The push() method adds one or more elements to the end of an array and returns the new length of the array.
+        fs.writeFile('./db/db.json', JSON.stringify(postNote), err => {//The JSON.stringify() method converts a JavaScript object or value to a JSON string, 
+                                                                       //optionally replacing values if a replacer function is specified or optionally including
+                                                                       // only the specified properties if a replacer array is specified.
             if (err) throw err;
-            res.send(notes)
+            res.send(notes)//post request to the page. Routes HTTP POST requests to the specified path with the specified callback functions. 
         });
     });
 });
@@ -37,7 +42,8 @@ router.delete('/notes/:id', (req, res) => {
         if (err) throw err;
 
         const deleteNotes = JSON.parse(data);
-        const delFilteredNote = deleteNotes.filter((note) => note.id != deleteID);
+        const delFilteredNote = deleteNotes.filter((note) => note.id != deleteID);//The filter() method creates a new array with all elements 
+                                                                                  //that pass the test implemented by the provided function.
 
         fs.writeFile('./db/db.json', JSON.stringify(delFilteredNote), err => {
             if (err) throw err;
